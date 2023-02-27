a848985aa4b870b8847fda05ab85cb2b30dfaafc
User1618 <wesleyisilv@gmail.com>
2023-02-26 21:47:37 -0300

    Inicio Spring e Rest

---
+++ rest-springboot-udemy/src/main/java/com/br/ent/restspringbootudemy/RestSpringbootUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.br.ent.restspringbootudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringbootUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringbootUdemyApplication.class, args);
+	}
+
+}
---
+++ rest-springboot-udemy/src/test/java/com/br/ent/restspringbootudemy/RestSpringbootUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ent.restspringbootudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringbootUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
