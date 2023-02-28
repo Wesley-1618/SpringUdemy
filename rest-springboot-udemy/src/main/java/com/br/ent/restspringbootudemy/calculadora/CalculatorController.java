d0ee83da2166c28a14434e00a0b174884e0b0287
User1618 <wesleyisilv@gmail.com>
2023-02-27 21:29:48 -0300

    ajustes na calculadora, modularizando e organizando o codigo

--- rest-springboot-udemy/src/main/java/com/br/ent/restspringbootudemy/calculadora/CalculatorController.java
+++ rest-springboot-udemy/src/main/java/com/br/ent/restspringbootudemy/calculadora/CalculatorController.java
@@ -5,30 +5,56 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.br.ent.restspringbootudemy.exception.UnsuportedMathOperationException;
+import com.br.ent.restspringbootudemy.utils.NumberUtils;
 
 @RestController
+@RequestMapping("/calculadora")
 public class CalculatorController {
 
-	@RequestMapping(value = "/calculadora/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-	public Double calculadora(@PathVariable("numberOne") String n1, @PathVariable("numberTwo") String n2) throws Exception {
-		if(!isNumeric(n1) || !isNumeric(n2)) {
-			throw new UnsuportedMathOperationException("Please set a numeric value!");
-		}
-		Double sum = convertToDouble(n1) + convertToDouble(n2);
-		return sum;
+	NumberUtils nUtil = new NumberUtils();
+
+	MathOperations math = new MathOperations();
+
+	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double calculadoraSoma(@PathVariable("numberOne") String n1, @PathVariable("numberTwo") String n2)
+			throws Exception {
+		NumberUtils.isNumeric(n1);
+		NumberUtils.isNumeric(n2);
+		return math.soma(NumberUtils.convertToDouble(n1), NumberUtils.convertToDouble(n2));
 	}
 
-	private Double convertToDouble(String number) {
-		if(number == null) return 0D;
-		String n = number.replaceAll(",", ".");
-		if (isNumeric(number)) return Double.parseDouble(n);
-		return 0D;
+	@RequestMapping(value = "/sub/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double calculadoraSub(@PathVariable("numberOne") String n1, @PathVariable("numberTwo") String n2) {
+		NumberUtils.isNumeric(n1);
+		NumberUtils.isNumeric(n2);
+		return math.subtracao(NumberUtils.convertToDouble(n1), NumberUtils.convertToDouble(n2));
 	}
 
-	private boolean isNumeric(String number) {
-		if(number == null) return false;
-		String n = number.replaceAll(",", ".");
-		return n.matches("[-+]?[0-9]*\\.?[0-9]+");
+	@RequestMapping(value = "/mult/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double calculadoraMult(@PathVariable("numberOne") String n1, @PathVariable("numberTwo") String n2) {
+		NumberUtils.isNumeric(n1);
+		NumberUtils.isNumeric(n2);
+		return math.multiplicacao(NumberUtils.convertToDouble(n1), NumberUtils.convertToDouble(n2));
 	}
+
+	@RequestMapping(value = "/media/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double calculadoraMedia(@PathVariable("numberOne") String n1, @PathVariable("numberTwo") String n2) {
+		NumberUtils.isNumeric(n1);
+		NumberUtils.isNumeric(n2);
+		return math.media(NumberUtils.convertToDouble(n1), NumberUtils.convertToDouble(n2));
+	}
+
+	@RequestMapping(value = "/div/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double calculadoraDiv(@PathVariable("numberOne") String n1, @PathVariable("numberTwo") String n2) {
+		NumberUtils.isNumeric(n1);
+		NumberUtils.isNumeric(n2);
+		return math.divisao(NumberUtils.convertToDouble(n1), NumberUtils.convertToDouble(n2));
+	}
+
+	@RequestMapping(value = "/raiz2/{numberOne}", method = RequestMethod.GET)
+	public Double calculadoraRaiz2(@PathVariable("numberOne") String n1) {
+		NumberUtils.isNumeric(n1);
+		return math.raizQuadrada(NumberUtils.convertToDouble(n1));
+	}
+
 }
