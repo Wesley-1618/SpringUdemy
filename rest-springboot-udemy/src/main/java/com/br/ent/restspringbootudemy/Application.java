a1c07aac45179f848a6d5f1c6d34e94a7f26486f
User1618 <wesleyisilv@gmail.com>
2023-04-28 15:34:48 -0300

    Upload e download de arquivos

--- rest-springboot-udemy/src/main/java/com/br/ent/restspringbootudemy/Application.java
+++ rest-springboot-udemy/src/main/java/com/br/ent/restspringbootudemy/Application.java
@@ -3,10 +3,16 @@ package com.br.ent.restspringbootudemy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.br.ent.restspringbootudemy.config.FileStorageConfig;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+	FileStorageConfig.class
+})
 @EnableAutoConfiguration
 @ComponentScan
 public class Application {
